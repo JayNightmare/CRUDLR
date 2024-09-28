b95caaeb2d35ec754d1e2ae625f12571a605775c
Jay <jordanbell321@gmail.com>
2024-09-28 22:24:29 +0100

    "Updated styles for ModuleList component container"

--- src/components/entity/modules/ModuleList.js
+++ src/components/entity/modules/ModuleList.js
@@ -18,10 +18,18 @@ const ModuleList = ({ modules, onSelect, isLoading }) => {
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        padding: 20,
+        // padding: 20,
         backgroundColor: "#fff",
         borderBottomLeftRadius: 5,
         borderBottomRightRadius: 5,
+        paddingLeft: 20,
+        paddingRight: 20,
+        marginBottom: 10,
+        shadowColor: "#000",
+        shadowOffset: { width: 0, height: 2 },
+        shadowOpacity: 0.25,
+        shadowRadius: 4,
+        elevation: 2,
     }
 });
 
